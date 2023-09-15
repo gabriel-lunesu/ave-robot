aeef6fb04fd6b07cbb25814bba9dfa9aa79101bc
gabriel-lunesu <2206751lunesu@zuyd.nl>
2023-09-15 14:32:12 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is the main script for the LEGO robot for this school project.
+# DEVS; 
+# Gabriel Lunesu, Yasin Leclaire, Tom Creemers, ...
+# Start [15 sep 2023]
+# Last edited [15 sep 2023]
+# Last important change: ""(KEEP EMPTY IF NOTHING TO NOTE)
+
+def main():
+    print("dit is een test")
+    
+print("main")
