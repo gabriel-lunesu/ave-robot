8f66a2f2db480dc755ec1f0858812dfa10977f70
gabriel-lunesu <2206751lunesu@zuyd.nl>
2023-10-06 11:10:08 +0200

    robot sensor cide
    gab

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # DEVS; 
 # Gabriel Lunesu, Yasin Leclaire, Tom Creemers, ...
 # Start [15 sep 2023]
-# Last edited [15 sep 2023]
+# Last edited [6 oct 2023]
 # Last important change: ""(KEEP EMPTY IF NOTHING TO NOTE)
 
 def main():
-    print("dit is een test")
+    print("dit is een test12345")
     
 print("main")
