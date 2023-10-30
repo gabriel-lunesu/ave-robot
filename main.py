aa3176b71339eeb1c7456ac04107dd4f8c6ba5db
gabriel-lunesu <2206751lunesu@zuyd.nl>
2023-10-30 11:47:05 +0100

    robot can follow black line and stop on red

--- main.py
+++ main.py
@@ -5,7 +5,26 @@
 # Last edited [6 oct 2023]
 # Last important change: ""(KEEP EMPTY IF NOTHING TO NOTE)
 
-def main():
-    print("dit is een test12345")
+from hub import light_matrix
+import runloop
+import motor_pair
+import color_sensor
+import math
+import time
+from hub import port
+import color
+
+async def main():
     
-print("main")
+    await light_matrix.write("Hi!")
+    motor_pair.pair(motor_pair.PAIR_1,port.C,port.D)
+    start_time = time.ticks_ms()
+    while time.ticks_ms() - start_time < 50000:
+        motor_pair.move(motor_pair.PAIR_1,(math.floor(-3/5)*color_sensor.reflection(port.F)+30),velocity=170)
+       
+        if color_sensor.color(port.F) is color.RED:
+            motor_pair.move(motor_pair.PAIR_1, 0, velocity=0)
+            
+    motor_pair.stop(motor_pair.PAIR_1)
+
+runloop.run(main())
